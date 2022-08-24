20d936e6aceb5289d7e431c465d591394f21cef7
jace.e94 <jace.edwards94@gmail.com>
2022-08-23 23:10:05 -0600

    hashed passwords for user creation and updating

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,7 +5,7 @@ const { User } = require('../../models');
 router.get('/', (req, res) => {
   // Access our User model and run .findAll() method)
     User.findAll({
-        attributes: { exclude: ['password']}
+        //attributes: { exclude: ['password']}
     })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     
     User.findOne({
-        attributes: { exclude: ['password']},
+        //attributes: { exclude: ['password']},
         where: {
             id: req.params.id
         }
@@ -62,9 +62,10 @@ router.put('/:id', (req, res) => {
   
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
     User.update(req.body, {
-      where: {
-        id: req.params.id
-      }
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
     })
     .then(dbUserData => {
         if (!dbUserData[0]) {
